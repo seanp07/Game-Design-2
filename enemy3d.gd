d185129c8433fd03cef6b958425c50399ad96ef2
seanp07 <157421441+seanp07@users.noreply.github.com>
2024-03-11 15:29:50 -0500

    Update 3/11

---
+++ enemy3d.gd
@@ -0,0 +1,22 @@
+extends CharacterBody3D
+
+@onready var dmg_area = $DamageArea
+@onready var atk_area = $AttackArea
+@onready var nav_agent = $NavigationAgent3D
+
+var SPEED = 3.0
+var ACCEL = 20.0
+var ATTACK = 10
+var knockback = 32
+
+func _physics_process(delta):
+	for player in get_tree().get_nodes_in_group("Player"):
+		if $AttackRange.overlaps_body(player):
+			nav_agent.target_position = player.global_position
+		if atk_area.overlaps_body(player):
+			player.take_damage(ATTACK)
+			var inert = player.global_position - self.global_position
+			player.inertia = inert.normalized() * knockback
+		var dir = (nav_agent.target_position - self.global_position).normalized()
+		velocity = velocity.lerp(dir * SPEED, ACCEL * delta)
+		move_and_slide()
---
+++ playerhud3d.gd
@@ -0,0 +1,6 @@
+extends Control
+
+@onready var dmg_overlay = $overlay
+@onready var healthbar = $healthbar
+@onready var popup = $popup
+@onready var popup_label = $popup/Label
