a6469c7fafdd265519c57c37835bfa7e1c89d067
seanp07 <157421441+seanp07@users.noreply.github.com>
2024-04-16 15:25:06 -0500

    4/16

--- racegoal.gd
+++ racegoal.gd
@@ -5,4 +5,9 @@ func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		laps += 1
 	if laps == 2:
-		OS.alert("You win!")
+		if "2" in get_tree().current_scene.name:
+			OS.alert("You win!")
+			get_tree().change_scene_to_file("res://rc_world_3.tscn")
+		else:
+			OS.alert("You win!")
+			get_tree().change_scene_to_file("res://rc_world_2.tscn")
