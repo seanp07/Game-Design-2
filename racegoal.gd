16169c163a6e57b18179d857dd9f8bbfea89182a
seanp07 <157421441+seanp07@users.noreply.github.com>
2024-04-18 15:13:00 -0500

    4/18

--- racegoal.gd
+++ racegoal.gd
@@ -4,7 +4,7 @@ extends Node3D
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		laps += 1
-	if laps == 2:
+	if laps == 4:
 		if "2" in get_tree().current_scene.name:
 			OS.alert("You win!")
 			get_tree().change_scene_to_file("res://rc_world_3.tscn")
