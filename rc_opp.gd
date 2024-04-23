2a2ef36beece2b338a0046e1911d66406349c757
seanp07 <palmer.s@stu.janesville.k12.wi.us>
2024-04-23 15:25:48 -0500

    4/23

--- rc_opp.gd
+++ rc_opp.gd
@@ -1,9 +1,9 @@
 extends VehicleBody3D
 
 const MAX_STEER = 0.4
-const MAX_RPM = 150
-const MAX_TORQUE = 500
-const HORSE_POWER = 75
+const MAX_RPM = 300
+const MAX_TORQUE = 200
+const HORSE_POWER = 100
 const REVERSE_POWER = -HORSE_POWER * 2 
 const STUCK_TIME_THRESHOLD = 0.5
 
@@ -17,9 +17,24 @@ var is_stuck = 0
 @onready var rayR = $rayright 
 
 func _physics_process(delta):
-	pass
-
-
+	var target_steering = 0.0
+	update_raycasts()
+	
+	var acceleration = HORSE_POWER 
+	if rayF.is_colliding():
+		var distance_to_obstacle = rayF.get_collision_point().distance_to(self.global_transform.origin)
+		acceleration *= max(0.1, distance_to_obstacle / 10)
+	
+	if rayFL.is_colliding() or rayL.is_colliding():
+		target_steering -= MAX_STEER
+	if rayFR.is_colliding() or rayR.is_colliding():
+		target_steering += MAX_STEER
+	
+	if is_stuck:
+		acceleration = REVERSE_POWER
+		steering = -sign(steering) * MAX_STEER
+	$backleft.engine_force = calc_engine_force(acceleration, abs($backleft.get_rpm()))
+	$backright.engine_force = calc_engine_force(acceleration, abs($backright.get_rpm()))
 func check_stuck_condition(delta):
 	if linear_velocity.length() < 1.0:
 		stuck_timer += delta
